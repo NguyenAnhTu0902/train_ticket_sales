a65a939bb4689cff488b081becb8c4755913848a
Amira Tu Nguyen <kinganhtu0902@gmail.com>
2025-11-07 10:20:51 +0700

    custom dynamic middleware for api key and permission

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,12 @@
 
 const express = require("express");
 const router = express.Router();
+const { apiKey, permission } = require("../auth/checkAuth");
+//check api key
+router.use(apiKey);  
+
+//check permissions
+router.use(permission("0000"));
 
 router.use("/v1/api", require("./auth"));
 
---
+++ src/services/apiKey.service.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const ApiKeyModel = require("../models/apiKey.model");
+const crypto = require("crypto");
+
+class ApiKeyService {
+  static findById = async (key) => {
+    const newKey = await ApiKeyModel.create({
+      key: crypto.randomBytes(64).toString("hex"),
+      permissions: ["0000"],
+    });
+    console.log("newKey::", newKey);
+    
+    const apiKey = await ApiKeyModel.findOne({key, status: true}).lean();
+    return apiKey;
+  };
+}
+
+module.exports = ApiKeyService;
